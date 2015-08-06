e133e5bc27edd11a07bc7debfeddaec74e96e265
Kevin Albertson <kevin.albertson@10gen.com>
2015-08-06 10:18:58 -0400

    Adds unit test

--- src/mongo/db/geo/s2_fassert_test.cpp
+++ src/mongo/db/geo/s2_fassert_test.cpp
@@ -1,11 +1,29 @@
+#include <iomanip>
+
 #include "mongo/unittest/unittest.h"
+#include "geoparser.h"
 #include "third_party/s2/s2.h"
 #include "third_party/s2/s2latlng.h"
 
 namespace {
     TEST(S2Fassert, Ticket) {
-        S2LatLng a(1.838544019, 89.73816011);
-        S2LatLng b(-45, 0);
-        S2LatLng c(-45, 0);
+        S2Point out;
+        S2LatLng ll = S2LatLng::FromDegrees(0, -45).Normalized();
+        out = ll.ToPoint();
+        std::cout << std::setprecision(100) << out.x() << std::endl;
+        std::cout << std::setprecision(100) << out.y() << std::endl;
+        std::cout << std::setprecision(100) << out.z() << std::endl;
+
+        std::cout << "Binary representation of out.x() is" << std::endl;
+
+        union Rep {
+            double dValue;
+            uint64_t iValue;
+        };
+
+        Rep outUnion;
+        outUnion.dValue= out.x();
+        std::cout << outUnion.iValue;
+        std::cout << std::endl;
     }
 }
